3fef30b93a7520810595df24e2ce302a4f9e8f02
fotis2222 <nitsas2012@gmail.com>
2024-07-28 13:28:28 +0300

    update stuff

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,8 @@
 	let level = 1;
 	let levelReq = 5;
 	let xpMulti = 1;
+	let pp = 0;
+	let ppMulti = 1;
 
 	function getPower() {
 		power += powerMulti;
@@ -52,6 +54,16 @@
 	function buy(upgrade) {
 		upgrade.buyUpg()
 	}
+
+	function energize() {
+		power = 0;
+		powerMulti = 1;
+		xp = 0;
+		level = 1;
+		levelReq = 5;
+		xpMulti = 1;
+		pp += ppMulti * (level - 1);
+	}
 </script>
 
 <h1>Incremental power!</h1>
@@ -61,9 +73,10 @@
 <button on:click={() => buy(upgrades.power_1)} class="power-button">Power I<br>+100% power earned.<br>Cost: {upgrades.power_1.cost}<br>Level: {upgrades.power_1.level}</button>
 <button on:click={() => buy(upgrades.xp_1)} class="power-button">XP I<br>+100% XP earned.<br>Cost: {upgrades.xp_1.cost}<br>Level: {upgrades.xp_1.level}</button>
 <hr>
-<h2>Energize(DOESN'T WORK)</h2>
+<h2>Energize</h2>
 <p>Energize will reset everything so far for energy.</p>
 <button class="energize-button">Do it for {level - 1} energy</button>
+<p>{pp} PP</p>
 <hr>
 <h2>Credits</h2>
 <p>Code: me</p>
